ccfb4a9cbeb5c5e730b43a86e134a38b15548579
mortiz275 <46685232+mortiz275@users.noreply.github.com>
2020-01-23 14:41:51 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
+//Basic Hello World Program
 public class HelloWorld{
   public static void main(String[] args){
-    String world= "Hello World!";
-    System.out.println(world);
+    String world= "Hello World!";//This could be ommited
+    System.out.println(world);//("Hello World!") instead of variable
     }
 }
